2e8e9d594e05b6b683d8d1cb0a58457b990c02ea
Fleshborne <blakeathompson94@gmail.com>
2020-09-08 16:00:55 -0400

    removed nav toggler for now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,16 +6,6 @@ export default function NavBar() {
   return (
     <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark' sticky='top'>
       <Navbar.Brand href='#home'>Welcome!</Navbar.Brand>
-      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-      <Navbar.Collapse id='responsive-navbar-nav'>
-        <Nav className='mr-auto' />
-        <Nav>
-          <Nav.Link href='#About'>About</Nav.Link>
-          <Nav.Link href='#Projects'>Projects</Nav.Link>
-          <Nav.Link href='#Resume'>Resume</Nav.Link>
-          <Nav.Link href='#Contact'>Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
     </Navbar>
   );
 }
